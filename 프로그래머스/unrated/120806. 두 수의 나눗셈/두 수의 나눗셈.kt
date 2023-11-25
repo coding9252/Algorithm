634859d8adb1c017961d47abe242ce5251fef250
Gahyeon Lee <150312899+coding9252@users.noreply.github.com>
2023-11-25 23:23:53 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 60.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(num1: Int, num2: Int) = num1 * 1000 / num2
+}
+
+// class Solution {
+//     fun solution(num1: Int, num2: Int): Int {
+//         var answer: Int = 0
+//         answer = (num1.toDouble() / num2.toDouble() * 1000).toInt()
+//         return answer
+//     }
+// }
